ad6406e5e890db516f956d89ab42d0dce028bd4a
luminous-badger <luminous.badger@gmail.com>
2018-09-14 09:59:33 +0100

    New files 09 2018

--- onzpwr.py
+++ onzpwr.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-# Julia set for any Z.
-# JM Sat 28 Apr 2018 13:04:05 BST
+# Attempt to do a Newton fractal outline in Python.
+# For any value of ZPower.
+# JM Thu 16 Aug 2018 14:02:49 BST
 
 from PIL import Image
 from PIL import ImageFont
@@ -9,54 +10,51 @@ from PIL import ImageDraw
 import numpy as nm
 import cmath
 from timeit import default_timer as timer
-from lc import colour_list 
+from lc import colour_list
 
 start = timer()
 
-X_MIN = -1.5
-X_MAX =  1.5
-Y_MIN = -1.5
-Y_MAX =  1.5
-offset     = 0.01
-maxiter    = 200
+X_MIN   = -2.0
+X_MAX   =  2.0
+Y_MIN   = -2.0
+Y_MAX   =  2.0
+offset  =  0.01
+epsilon = 0.01
+maxiter = 50
 calc_count = 0
 rnum       = 93
-ZPower     = 2.0
-C          = complex ( -0.721, 0.2951 )
+lenlc      = len( colour_list )  
+ZPower     = 3.5
 
-lenlc      = len( colour_list ) 
-
-# create a new X*Y pixel image surface
-# make the background white (default bg=black)
 X_SIZE = ( X_MAX - X_MIN ) / offset
 Y_SIZE = ( Y_MAX - Y_MIN ) / offset
 
 X_SIZE += 1
 Y_SIZE += 1
-
-X_SIZE = int( X_SIZE )
-Y_SIZE = int( Y_SIZE )
-
+X_SIZE  = int( X_SIZE )
+Y_SIZE  = int( Y_SIZE )
 print 'X: ', X_SIZE ,' Y: ', Y_SIZE 
 
 white      = (255,255,255)
-randcolour =  ( 135,206,250) 
-blue       = (0,0,255) 
+randcolour = ( 190, 190, 190 )
 img        = Image.new( "RGB", [ X_SIZE, Y_SIZE ], white )
+mycolour   = ( 100, 150, 200 ) 
 
-mycolour = ( 100, 150, 200 ) 
 prev_iter = 0
 x_pixel = 0
 for X in nm.arange ( X_MIN, X_MAX, offset ):
 	y_pixel = 0
 	for Y in nm.arange ( Y_MIN, Y_MAX, offset ):
+		#Z = complex ( 0, 0 )
 		Z = complex ( X, Y )
 		iter_count = 0
 
-		while ( abs ( Z**ZPower ) < 4 and iter_count < maxiter ):
-			Z = Z**ZPower + C
+		while ( abs ( Z**ZPower - 1 ) > epsilon and iter_count < maxiter ):
+			if ( Z ):
+				Z =  Z - ( Z**ZPower - 1 ) / ( ZPower * Z**( ZPower - 1 ) ) 
+
 			iter_count = iter_count + 1
-			#print X, Y , Z
+		
 			calc_count = calc_count + 1  
 		if ( prev_iter == iter_count ):
 			img.putpixel( ( x_pixel,  y_pixel ), white ) 
@@ -64,19 +62,25 @@ for X in nm.arange ( X_MIN, X_MAX, offset ):
 			img.putpixel( ( x_pixel,  y_pixel ), mycolour ) 
 		prev_iter = iter_count
 		y_pixel += 1
-
+		
 	x_pixel += 1
 
 dt = timer() - start
 
-MsgText = 'Outline Julia for Z^' + str( ZPower ) + str( C ) + ' and rnum: ' + str( rnum )
-fname = 'OL_Julia_X:' + str( X_MAX ) + str( X_MIN ) + '_Y:' + str( Y_MAX ) + str( Y_MIN ) + str( C ) + '.png'
+MsgText = 'Outline Newton for Z^' + str( ZPower ) 
+if ( X_SIZE < 1000 ):
+	print 'Less'
+	fontsize = 12
+else:
+	print 'Grts'
+	fontsize = 32
 
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype( "/Library/Fonts/Arial.ttf", 12 )
+font = ImageFont.truetype( "/Library/Fonts/Arial.ttf", fontsize )
 draw.text( ( 0, 0 ),  MsgText, ( 139,0,0 ), font=font )
 
-print MsgText, 'created in %f s' % dt
+fname = 'Outline_Newton_Z' + str( ZPower ) + '_' + str( offset ) + '.png'
+print 'Newton for Z^', ZPower, ' and Rand:', rnum, 'created in %f s' % dt
 print 'Calc: ', calc_count
 print 'Fname:', fname
 
